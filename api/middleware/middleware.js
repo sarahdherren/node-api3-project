62b165bc2b77202590616f498a5d7733d7c992dd
Sarah D Herren <herren.sarah@icloud.com>
2021-09-29 16:32:38 -0500

    added validateUserId and validateUser middlewares

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,15 +1,38 @@
+const Users = require('../users/users-model');
+
 function logger(req, res, next) {
   const timeStamp = new Date()
   console.log(req.method, req.originalUrl, timeStamp)
   next()
 }
 
-function validateUserId(req, res, next) {
-  
+async function validateUserId(req, res, next) {
+  const { id } = req.params
+  try {
+  const user = await Users.getById(id)
+  if(user) {
+    req.user = user
+  } else {
+    next({
+      status: 404,
+      message: "user not found"
+    })
+  } 
+  } catch (error) {
+    next(error)
+  }
 }
 
 function validateUser(req, res, next) {
-  
+  const { name } = req.body
+  if(!name || typeof name !== 'string' || !name.trim()) {
+    next({
+      status: 400,
+      message: "missing required name field"
+    })
+  }else {
+    next()
+  }
 }
 
 function validatePost(req, res, next) {
