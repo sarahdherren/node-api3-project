48661d540cecc1521a64ef2b457c70a74ebe0e0e
Sarah D Herren <herren.sarah@icloud.com>
2021-09-29 15:17:45 -0500

    added helmet to dependencies, wired up server iexported and imported middleware where required, wrote land using logger middleware

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,17 +1,24 @@
 function logger(req, res, next) {
-  // DO YOUR MAGIC
+  const timeStamp = new Date()
+  console.log(req.method, req.originalUrl, timeStamp)
+  next()
 }
 
 function validateUserId(req, res, next) {
-  // DO YOUR MAGIC
+  
 }
 
 function validateUser(req, res, next) {
-  // DO YOUR MAGIC
+  
 }
 
 function validatePost(req, res, next) {
-  // DO YOUR MAGIC
+  
 }
 
-// do not forget to expose these functions to other modules
+module.exports = {
+  logger,
+  validateUserId,
+  validateUser,
+  validatePost
+};
